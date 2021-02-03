c74ffa941c85a38d606083003cebadfa2abb1aa6
todcnst1453@gmail.com <todcnst1453@gmail.com>
2021-02-03 16:07:14 +0800

    test

--- eth/CzzToken.sol
+++ eth/CzzToken.sol
@@ -9,7 +9,7 @@ abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
         return msg.sender;
     }
-
+asdf
     function _msgData() internal view virtual returns (bytes memory) {
         this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
