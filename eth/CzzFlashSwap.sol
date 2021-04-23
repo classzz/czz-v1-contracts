c984aab8ce1ca53cead2831378715cab107e5590
a <a@a.a>
2021-04-23 16:19:10 +0800

    update code

---
+++ eth/CzzFlashSwap.sol
@@ -0,0 +1,132 @@
+pragma solidity =0.6.6;
+
+import './IERC20.sol';
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+interface ICzzSwap is IERC20 {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _account, uint256 _amount) external;
+    function transferOwnership(address newOwner) external;
+}
+
+
+contract HtV6Router is Ownable {
+    address czzToken;
+    mapping (address => uint8) private managers;
+    
+    
+    event MintToken(
+        address indexed to,
+        uint256 amount,
+        uint256 mid,
+        uint256 amountIn
+    );
+    event BurnToken(
+        address  indexed to,
+        uint256  amount,
+        uint256  ntype,
+        string   toToken
+    );
+    
+    constructor(address _token) public {
+        czzToken = _token;
+    }
+    
+    modifier isManager {
+        require(
+            msg.sender == owner() || managers[msg.sender] == 1);
+        _;
+    }
+    
+    function addManager(address manager) public onlyOwner{
+        managers[manager] = 1;
+    }
+    
+    function removeManager(address manager) public onlyOwner{
+        managers[manager] = 0;
+    }
+    
+    function setCzzTonkenAddress(address addr) public isManager {
+        czzToken = addr;
+    }
+
+    function getCzzTonkenAddress() public view isManager returns(address ){
+        return czzToken;
+    }
+    
+    function burn( uint _amountIn, uint256 ntype, string memory toToken) payable public 
+    {
+        address czzToken1 = czzToken;
+        ICzzSwap(czzToken1).burn(msg.sender, _amountIn);
+        emit BurnToken(msg.sender, _amountIn, ntype, toToken);
+    }
+
+    function mint(uint256 mid, address fromToken, uint256 _amountIn)  payable public isManager 
+    {
+        address czzToken1 = czzToken;
+        ICzzSwap(czzToken1).mint(fromToken, _amountIn);
+        emit MintToken(fromToken, 0, mid,_amountIn);
+    }
+}
